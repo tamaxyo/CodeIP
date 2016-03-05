9da0a89ed29c7e1fa8541f77f5fc67723bac69d7
tamaxyo <tamaxyo@gmail.com>
2016-03-05 18:24:41 +0900

    update badge when browser launched and when window selection changed

--- js/background.js
+++ js/background.js
@@ -1,5 +1,22 @@
 (function(){
+  var tabSelectionChanged = function() {
+    chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    }, function(tabs){
+      if(tabs.length > 0) {
+        chrome.browserAction.setBadgeText({text:tabs[0].id+""})        
+      }
+    });
+  }
+  
   chrome.tabs.onActivated.addListener(function(activeTab){
-    chrome.browserAction.setBadgeText({text:activeTab.tabId+""});
-  })
+    tabSelectionChanged();
+  });
+
+  chrome.windows.onFocusChanged.addListener(function(windowId){
+    tabSelectionChanged();
+  });
+
+  tabSelectionChanged();
 })();
