d8ff12ed402b414be133a35565a829eef251bfb2
tamaxyo <tamaxyo@gmail.com>
2016-03-06 13:09:29 +0900

    sophisticated notification

--- js/background.js
+++ js/background.js
@@ -1,22 +1,138 @@
 (function(){
+  var SERVER = "http://133.130.89.148/";
+
+  var uid = (function () {
+    var id = window.localStorage.getItem("id");
+    if(!id) {
+      id = guid();
+      window.localStorage.setItem("id", id);
+    }
+    return id;
+  })();
+
+  var update = function() {
+    tabSelectionChanged();
+    notifyUpdate();
+  }
   var tabSelectionChanged = function() {
+    var data = {};
+    data["pf"] = "Web";
+
     chrome.tabs.query({
       active: true,
       lastFocusedWindow: true
     }, function(tabs){
       if(tabs.length > 0) {
-        chrome.browserAction.setBadgeText({text:tabs[0].id+""})        
+        data["url"] = tabs[0].url
+
+        $.ajax({
+          method: "GET",
+          url: SERVER+"array",
+          data: JSON.stringify(data)
+        }).done(function(memos){
+          console.log("getLogs - success, data:", memos);
+
+          memos = memos.filter(function(m){
+            return data["url"] === m.url;
+          });
+          
+          if(memos.length > 0) {
+            chrome.browserAction.setBadgeBackgroundColor({color:[255, 187, 153, 255]});
+            chrome.browserAction.setBadgeText({text:memos.length+""});
+          } else {
+            chrome.browserAction.setBadgeText({text:"-"});
+            chrome.browserAction.setBadgeBackgroundColor({color: [200, 200, 200, 255]});
+          }
+        });
       }
     });
   }
+
+  var notifyUpdate = function() {
+    console.log("notifyUpdate");
+
+    var data = {};
+    data["uid"] = uid;
+    data["pf"] = "Web";
+
+    chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    }, function(tabs){
+      if(tabs.length > 0) {
+        data["url"] = tabs[0].url
+        var reqList = [];
+        reqList.push($.ajax({method: "GET", url:SERVER+"array", data: JSON.stringify(data)}));
+        reqList.push($.ajax({method: "GET", url:SERVER+"parray", data: JSON.stringify(data)}));
+        reqList.push($.ajax({method: "GET", url:SERVER+"rarray", data: JSON.stringify(data)}));
+
+        $.when.apply($, reqList).done(function() {
+          var memos = arguments[0][0].filter(function(m){ return data["url"] === m.url });
+          var rels = arguments[2][0].filter(function(r){
+            return $.inArray(r.bid, memos.map(function(m){ return m.id })) > -1
+          });
+          var projects = arguments[1][0].filter(function(p){
+            return $.inArray(p.id, rels.map(function(r){ return r.pid})) > -1
+          });
+
+          console.log("notifyUpdate - selcted projects: ", projects);
+          if(projects.length > 0) {
+            $.each(projects, function(idx){
+              chrome.notifications.create("", {
+                type: "basic",
+                iconUrl: "icon.png", 
+                title: "CodeIP", 
+                message: "project " + projects[idx].title + " is updated!!"
+              }, function(nid) {
+                console.log("notifyUpdate - sent");
+                sessionStorage.setItem(nid, projects[idx].url);
+                setTimeout(function(){
+                  console.log("notifyUpdate - cleared");
+                  chrome.notifications.clear(nid);
+                }, 5000);
+              })
+            })
+          }
+        });
+      }
+    });
+  }
+
+  chrome.notifications.onClicked.addListener(function(nid){
+    console.log("notifications.onClicked", nid);
+    var url = sessionStorage.getItem(nid);
+    if(url) {
+      console.log("open project page from notification", url)
+      chrome.tabs.create({url: url, active: true});
+    }
+  });
+
+  chrome.notifications.onClosed.addListener(function(nid){
+    console.log("notifications.onClosed");
+    sessionStorage.removeItem(nid);
+  })
   
   chrome.tabs.onActivated.addListener(function(activeTab){
-    tabSelectionChanged();
+    console.log("tabs.onActivated");
+    update();
+  });
+
+  chrome.tabs.onUpdated.addListener(function(activeTab){
+    console.log("tabs.onUpdated");
+    update();
   });
 
   chrome.windows.onFocusChanged.addListener(function(windowId){
-    tabSelectionChanged();
+    console.log("windows.onFocusChanged");
+    update();
   });
 
-  tabSelectionChanged();
+  chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
+    console.log("onMessage", req, sender);
+    if(req.method === "update" ) {
+      update();
+    }
+  });
+
+  update();
 })();
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 (function(){
+  var SERVER = "http://133.130.89.148/";
+
   var uid = (function () {
     var id = window.localStorage.getItem("id");
 
@@ -19,6 +21,9 @@
 
   var getMemos = function() {
     var data = {};
+    data["uid"] = uid;
+    data["pf"] = "Web";
+
     chrome.tabs.query({
       active: true,
       lastFocusedWindow: true
@@ -27,10 +32,15 @@
         data["url"] = tabs[0].url
         $.ajax({
           method: "GET",
-          url: "http://localhost:8000/memos.json",
+          url: SERVER+"array",
           data: JSON.stringify(data)
         }).done(function(memos){
           console.log("getLogs - success, data:", memos);
+
+          memos = memos.filter(function(m){
+            return data["url"] === m.url;
+          });
+          
           $("#memos").html(
             memos.map(function(m){
               return '<option value="' + m.memo + '"></option>';
@@ -47,29 +57,39 @@
 
   var getProjects = function() {
     var data = {};
+    data["uid"] = uid;
+    data["pf"] = "Web";
+
     chrome.tabs.query({
       active: true,
       lastFocusedWindow: true
     }, function(tabs){
       if(tabs.length > 0) {
         data["url"] = tabs[0].url
-        $.ajax({
-          method: "GET",
-          url: "http://localhost:8000/projects.json",
-          data: JSON.stringify(data)
-        }).done(function(projects){
-          console.log("getProjects - success, data:", projects);
-          $("#related").hide();
-          $("#projects").html(
-            projects.map(function(p){
-              return '<a target="_blank" href="' + p.url + '" title="' + p.description + '">' + p.title + '</a>'
-            }).join("<br />")
-          );
-          for(var i = 0; i < projects.length; i++) {
-            console.log(projects[i]);
-            $("#projects").append('<option value="' + projects[i].memo + '"></option>');
+        var reqList = [];
+        reqList.push($.ajax({method: "GET", url:SERVER+"array", data: JSON.stringify(data)}));
+        reqList.push($.ajax({method: "GET", url:SERVER+"parray", data: JSON.stringify(data)}));
+        reqList.push($.ajax({method: "GET", url:SERVER+"rarray", data: JSON.stringify(data)}));
+
+        $.when.apply($, reqList).done(function() {
+          var memos = arguments[0][0].filter(function(m){ return data["url"] === m.url });
+          var rels = arguments[2][0].filter(function(r){
+            return $.inArray(r.bid, memos.map(function(m){ return m.id })) > -1
+          });
+          var projects = arguments[1][0].filter(function(p){
+            return $.inArray(p.id, rels.map(function(r){ return r.pid})) > -1
+          });
+          
+          if (projects.length > 0) {
             $("#related").show();
-          }
+            $("#projects").html(
+              projects.map(function(p){
+                return '<a target="_blank" href="' + p.url + '" title="' + p.description + '">' + p.title + '</a>'
+              }).join("<br />")
+            );
+          } else {
+            $("#related").hide();
+          } 
         }).fail(function(xhr, status){
           console.log("getProjects - fail, status: ", status);
         }).always(function(){
@@ -79,19 +99,11 @@
     })
   };
 
-  var openProjectInTab = function(url) {
-    console.log("openProjectInTab", url)
-    chrome.tabs.create({
-      url: url, 
-      active: true
-    });
-  }
-  
   var sendMemo = function() {
     var data = {};
     data["uid"] = uid;
     data["pf"] = "Web";
-    data["memo"] = $("#memo").text();
+    data["memo"] = $("#memo").val();
     data["rate"] = $("#rate").val();
 
     chrome.tabs.query({
@@ -100,21 +112,21 @@
     }, function(tabs){
       if(tabs.length > 0) {
         data["url"] = tabs[0].url
-
+        console.log("sendMemo - ", data);
         $.ajax({
-          method: "GET", 
-          url: "http://localhost:8000",
-          data: JSON.stringify(data)
+          method: "POST", 
+          url: SERVER+"books/add",
+          data: data //JSON.stringify(data)
         }).done(function(){
           console.log("sendMemo - done");
-        }).fail(function(){
-          console.log("sendMemo - fail");
+        }).fail(function(xhr, status){
+          console.log("sendMemo - fail, status: ", status);
         }).always(function(){
           console.log("sendMemo - always")
+          chrome.runtime.sendMessage({method: "update"});
         });
       }
     });
-
   }
   
   $(function(){
